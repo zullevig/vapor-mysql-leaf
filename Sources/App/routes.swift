8f4ebcf917e0c45d2bc1b3b6faec8036750e69fa
Zachary Ullevig <zullevig@bignerdranch.com>
2019-09-20 12:35:38 -0600

    Added example api call

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -3,27 +3,32 @@ import Vapor
 /// Register your application's routes here.
 public func routes(_ router: Router) throws {
     // Basic "It works" example
-    router.get { req in
+    router.get { request in
         return "It works!"
     }
     
     // Basic "Hello, world!" example
-    router.get("hello") { req in
+    router.get("hello") { request in
         return "Hello, world!"
     }
     
-    router.post("testmodel") { req -> Future<Response> in
-        return try req.content.decode(TestModel.self).flatMap { test in
-            return test.save(on: req).map { _ in
-                return req.redirect(to: "testmodel")
-            }
+    router.get("testmodel") { request -> Future<[TestModel]> in
+        let models = TestModel.query(on: request).all()
+        return models
+    }
+
+    router.get("testweb") { request -> Future<View> in
+        return TestModel.query(on: request).all().flatMap { results in
+            let data = ["testlist": results]
+            return try request.view().render("testview", data)
         }
     }
     
-    router.get("testmodel") { req -> Future<View> in
-        return TestModel.query(on: req).all().flatMap { results in
-            let data = ["testlist": results]
-            return try req.view().render("testview", data)
+    router.post("testweb") { request in
+        return try request.content.decode(TestModel.self).flatMap { item in
+            return item.save(on: request).map { _ in
+                return request.redirect(to: "testweb")
+            }
         }
     }
 }
